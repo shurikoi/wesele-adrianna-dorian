4891f733da60261e61604762ceaac5d0d834ec11
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2025-04-21 19:48:38 +0200

    Implement authentication and authorization for guest access retrieval

--- wesele-app/app/api/getGuestAccesses/route.ts
+++ wesele-app/app/api/getGuestAccesses/route.ts
@@ -1,11 +1,16 @@
 import connection from "@/lib/connection";
 import GuestAccess from "@/models/GuestAccess";
 import { NextResponse } from "next/server";
+import { auth } from "@/auth"
 import "@/models/Guest";
 
 
 export async function GET() {
     try {
+        const session = await auth();
+        if (!session) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        if (session?.user?.role !== "admin") return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+        
         await connection();
         const guestAccesses = await GuestAccess.find({}).populate('guests');
         return NextResponse.json(guestAccesses, { status: 200 });
